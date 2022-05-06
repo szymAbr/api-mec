f9deb78db123505bb9072c6b43face50f90ca2e1
szymAbr <sz.abramowski@gmail.com>
2022-05-06 17:15:16 +0200

    update ports

--- server.js
+++ server.js
@@ -5,23 +5,7 @@ const app = express();
 const mongoose = require("mongoose");
 const ws = require("ws");
 const client = new ws("wss://mec-storage.herokuapp.com");
-//
-const wss = new ws.Server({ port: 8082 });
 
-wss.on("connection", (ws) => {
-  console.log("New client connected!");
-
-  ws.on("message", (data) => {
-    console.log(`Client has sent us: ${data}`);
-
-    ws.send(`Here's the data: ${data}`);
-  });
-
-  ws.on("close", () => {
-    console.log("Client has disconnected!");
-  });
-});
-//
 client.on("open", () => {
   console.log("Connected to websocket!");
 });
@@ -32,7 +16,9 @@ client.on("message", function message(data, isBinary) {
   console.log(message);
 });
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/api-mec", {
+  useNewUrlParser: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
@@ -45,4 +31,4 @@ app.use("/products", productsRouter);
 const ordersRouter = require("./routes/orders");
 app.use("/orders", ordersRouter);
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
