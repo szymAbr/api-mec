b6d9b09240fb4fd6bf666f05520accbf6d05bf46
szymAbr <sz.abramowski@gmail.com>
2022-05-06 18:32:45 +0200

    update mongodb uri

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ client.on("message", function message(data, isBinary) {
   console.log(message);
 });
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/api-mec", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
 });
 const db = mongoose.connection;
